6f83adcbc5c08fac476143b663cdd89c20ff8b68
cô giáo hưng <hungdang02042003@gmail.com>
2024-01-17 20:08:27 +0700

    install joi validate

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,7 +1,26 @@
+import Joi from 'joi';
 import Product from '../models/product.model.js';
 import express from 'express';
 
 const router = express.Router();
+// nameProduct = ''
+const schema = Joi.object({
+	nameProduct: Joi.string().required().messages({
+		'string.base': 'Tên sản phẩm phải là kiểu chuỗi',
+		'string.empty': 'Tên sản phẩm không được để trống',
+		'any.required': 'Tên sản phẩm là bắt buộc',
+	}),
+	price: Joi.number().required().messages({
+		'number.base': 'Giá sản phẩm phải là kiểu số',
+		'number.empty': 'Giá sản phẩm không được để trống',
+		'any.required': 'Giá sản phẩm là bắt buộc',
+	}),
+	imageProduct: Joi.string().required().messages({
+		'string.base': 'Hình sản phẩm phải là kiểu chuỗi',
+		'string.empty': 'Hình sản phẩm không được để trống',
+		'any.required': 'Hình sản phẩm là bắt buộc',
+	}),
+});
 
 router.get('/tat-ca-san-pham', async function (request, response) {
 	// request: là những gì người dùng gửi dữ liệu lên
@@ -14,6 +33,21 @@ router.get('/san-pham/:productId', async function (request, response) {
 	const { productId } = request.params;
 	const result = await Product.findById(productId);
 	return response.status(200).json(result);
+});
+
+router.post('/create-product', async function (request, response) {
+	const data = request.body;
+
+	const { error } = schema.validate(data, { abortEarly: false });
+	if (error) {
+		// [1, 2, 3]
+		return response
+			.status(400)
+			.json(error.details.map((item) => ({ message: item.message })));
+	}
+
+	// const result = await Product.create(data);
+	// console.log('🚀 ~ result:', result);
 });
 
 export default router;
